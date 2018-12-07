2e84235f9f86083aa0cb95dec909b7108be4a96f
zhujunjie <1732786398@qq.com>
2018-12-07 22:27:27 +0800

    实现了一个Socket服务器

---
+++ SocketServer1/SocketServerForm.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace SocketServer1
+{
+    partial class SocketServerForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_IP = new System.Windows.Forms.Label();
+            this.label_port = new System.Windows.Forms.Label();
+            this.textBox_IP = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.listenClient_btn = new System.Windows.Forms.Button();
+            this.listBox_receiveMsg = new System.Windows.Forms.ListBox();
+            this.sendMsg_btn = new System.Windows.Forms.Button();
+            this.textBox_sendMsg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label_IP
+            // 
+            this.label_IP.AutoSize = true;
+            this.label_IP.Location = new System.Drawing.Point(12, 25);
+            this.label_IP.Name = "label_IP";
+            this.label_IP.Size = new System.Drawing.Size(23, 12);
+            this.label_IP.TabIndex = 0;
+            this.label_IP.Text = "IP:";
+            this.label_IP.Click += new System.EventHandler(this.label_IP_Click);
+            // 
+            // label_port
+            // 
+            this.label_port.AutoSize = true;
+            this.label_port.Location = new System.Drawing.Point(333, 28);
+            this.label_port.Name = "label_port";
+            this.label_port.Size = new System.Drawing.Size(35, 12);
+            this.label_port.TabIndex = 1;
+            this.label_port.Text = "Port:";
+            // 
+            // textBox_IP
+            // 
+            this.textBox_IP.Location = new System.Drawing.Point(54, 25);
+            this.textBox_IP.Name = "textBox_IP";
+            this.textBox_IP.Text = "127.0.0.1";
+            this.textBox_IP.Size = new System.Drawing.Size(200, 21);
+            this.textBox_IP.TabIndex = 2;
+            this.textBox_IP.TextChanged += new System.EventHandler(this.textBox_IP_TextChanged);
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(390, 25);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Text = "50001";
+            this.textBox_port.Size = new System.Drawing.Size(232, 21);
+            this.textBox_port.TabIndex = 3;
+            this.textBox_port.TextChanged += new System.EventHandler(this.textBox_port_TextChanged);
+            // 
+            // listenClient_btn
+            // 
+            this.listenClient_btn.Location = new System.Drawing.Point(728, 6);
+            this.listenClient_btn.Name = "listenClient_btn";
+            this.listenClient_btn.Size = new System.Drawing.Size(148, 57);
+            this.listenClient_btn.TabIndex = 4;
+            this.listenClient_btn.Text = "开始监听";
+            this.listenClient_btn.UseVisualStyleBackColor = true;
+            this.listenClient_btn.Click += new System.EventHandler(this.listenClient_btn_Click);
+            // 
+            // listBox_receiveMsg
+            // 
+            this.listBox_receiveMsg.FormattingEnabled = true;
+            this.listBox_receiveMsg.ItemHeight = 12;
+            this.listBox_receiveMsg.Location = new System.Drawing.Point(29, 83);
+            this.listBox_receiveMsg.Name = "listBox_receiveMsg";
+            this.listBox_receiveMsg.Size = new System.Drawing.Size(847, 244);
+            this.listBox_receiveMsg.TabIndex = 5;
+            // 
+            // sendMsg_btn
+            // 
+            this.sendMsg_btn.Location = new System.Drawing.Point(728, 476);
+            this.sendMsg_btn.Name = "sendMsg_btn";
+            this.sendMsg_btn.Size = new System.Drawing.Size(148, 57);
+            this.sendMsg_btn.TabIndex = 6;
+            this.sendMsg_btn.Text = "发送";
+            this.sendMsg_btn.UseVisualStyleBackColor = true;
+            this.sendMsg_btn.Click += new System.EventHandler(this.sendMsg_btn_Click);
+            // 
+            // textBox_sendMsg
+            // 
+            this.textBox_sendMsg.Location = new System.Drawing.Point(29, 358);
+            this.textBox_sendMsg.Multiline = true;
+            this.textBox_sendMsg.Name = "textBox_sendMsg";
+            this.textBox_sendMsg.Size = new System.Drawing.Size(637, 175);
+            this.textBox_sendMsg.TabIndex = 7;
+            this.textBox_sendMsg.TextChanged += new System.EventHandler(this.textBox_sendMsg_TextChanged);
+            // 
+            // SocketServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 569);
+            this.Controls.Add(this.textBox_sendMsg);
+            this.Controls.Add(this.sendMsg_btn);
+            this.Controls.Add(this.listBox_receiveMsg);
+            this.Controls.Add(this.listenClient_btn);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.textBox_IP);
+            this.Controls.Add(this.label_port);
+            this.Controls.Add(this.label_IP);
+            this.Name = "SocketServerForm";
+            this.Text = "SocketServerForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_IP;
+        private System.Windows.Forms.Label label_port;
+        private System.Windows.Forms.TextBox textBox_IP;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Button listenClient_btn;
+        private System.Windows.Forms.ListBox listBox_receiveMsg;
+        private System.Windows.Forms.Button sendMsg_btn;
+        private System.Windows.Forms.TextBox textBox_sendMsg;
+    }
+}
+
---
+++ SocketServer1/SocketServerForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SocketServer1
+{
+    public partial class SocketServerForm : Form
+    {
+        public SocketServerForm()
+        {
+            InitializeComponent();
+        }
+        private delegate void showMsgDelegate(string str);//定义一个委托
+        private void showMsg(string str)
+        {
+            if (this.listBox_receiveMsg.InvokeRequired)
+            {
+                showMsgDelegate msgDelegate = new showMsgDelegate(showMsg);
+                this.listBox_receiveMsg.Invoke(msgDelegate, str);
+            }
+            else
+            {
+                this.listBox_receiveMsg.Items.Add(str + "\r\n");
+            }
+        }
+        //点击进行监听
+        private void listenClient_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //点击开始监听时 在服务端创建一个负责监听IP和端口号的Socket
+                Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Any;
+                //创建对象端口
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(textBox_port.Text.Trim()));
+
+                socketWatch.Bind(point);//绑定端口号
+                showMsg("监听成功!");
+                socketWatch.Listen(10);//设置监听
+
+                //创建监听客户端的线程
+                Thread thread = new Thread(Listen);
+                thread.IsBackground = true;
+                thread.Start(socketWatch);
+            } catch {
+                
+            }
+        }
+        //显示提示信息
+        public void ShowMsg(string msg)
+        {
+            listBox_receiveMsg.Items.Add(msg + "\r\n");//显示一行信息
+        }
+        public void Listen(object o)
+        {
+            try
+            {
+                Socket socketWatch = o as Socket;
+                while (true)
+                {
+                    Socket socketSend1 = socketWatch.Accept();//等待接收客户端连接
+                    socketSend = socketSend1;
+                    showMsg(socketSend.RemoteEndPoint.ToString() + ":" + "连接成功!");
+                    //开启一个新线程，执行接收消息方法
+                    Thread receiveClient_thread = new Thread(Received);
+                    receiveClient_thread.IsBackground = true;
+                    receiveClient_thread.Start(socketSend);
+                }
+            } catch (SocketException e){
+                Console.WriteLine(e.ToString());
+            }
+        }
+        /// <summary>
+        /// 服务器端不停的接收客户端发来的消息
+        /// </summary>
+        /// <param name="o"></param>
+        void Received(object o)
+        {
+            try
+            {
+                Socket socketSend = o as Socket;
+                while (true)
+                {
+                    //客户端连接服务器成功后，服务器接收客户端发送的消息
+                    byte[] buffer = new byte[1024 * 1024 * 5];
+                    //实际接收到的有效字节数
+                    int len = socketSend.Receive(buffer);
+                    Console.WriteLine(len);
+                    if (len == 0)
+                    {
+                        break;
+                    }
+                    string str = Encoding.UTF8.GetString(buffer, 0, len);
+                    Console.WriteLine(str);
+                    showMsg(socketSend.RemoteEndPoint + ":" + str);
+                }
+            }
+            catch { }
+        }
+        Socket socketSend;
+        void Send(string str)
+        {
+            byte[] msgBytes = Encoding.UTF8.GetBytes(str);
+            socketSend.Send(msgBytes);
+        }
+        private void sendMsg_btn_Click(object sender, EventArgs e)
+        {
+            Send(textBox_sendMsg.Text.Trim());
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_IP_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_port_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_sendMsg_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_IP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
